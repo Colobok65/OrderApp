25fb1becc7c098996180d97164d775a4fd07a8ba
Schur Artem <84aas@mail.ru>
2022-03-11 15:37:23 +0300

    was added password for using with docker and insert in import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -5,3 +5,9 @@ INSERT INTO user (id, address, login, password, username) VALUES (1, 'login1', '
 
 INSERT INTO user_authorities (user_id, authority_id) VALUES (1, 1);
 INSERT INTO user_authorities (user_id, authority_id) VALUES (1, 2);
+
+INSERT INTO goods (name, price) VALUES ('sofa', 3000);
+INSERT INTO goods (name, price) VALUES ('lamp', 300);
+INSERT INTO goods (name, price) VALUES ('brush', 50);
+INSERT INTO goods (name, price) VALUES ('toothpaste', 100);
+INSERT INTO goods (name, price) VALUES ('chair', 1200);
